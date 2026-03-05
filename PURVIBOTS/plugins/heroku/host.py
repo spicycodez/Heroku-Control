ff6bb94375931c708329e37cc24690744ae5ce40
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-03-05 14:54:31 +0530

    Update repository URLs for Heroku control

--- PURVIBOTS/plugins/heroku/host.py
+++ PURVIBOTS/plugins/heroku/host.py
@@ -15,10 +15,10 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 HEROKU_API_URL = "https://api.heroku.com"
 HEROKU_API_KEY = os.getenv("HEROKU_API_KEY")  # Pre-defined variable
-REPO_URL = "https://github.com/TEAMPURVI/PURVI_MUSIC"
+REPO_URL = "https://github.com/spicycodez/Heroku-Control"
 BRANCH_NAME = None
 BUILDPACK_URL = "https://github.com/heroku/heroku-buildpack-python"
-UPSTREAM_REPO = "https://github.com/TEAMPURVI/Heroku_Bot"  # Pre-defined variable
+UPSTREAM_REPO = "https://github.com/spicycodez/Heroku-Control"  # Pre-defined variable
 UPSTREAM_BRANCH = "master"  # Pre-defined variable
 API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
